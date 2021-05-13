b4fa46928316046acf380f9068e56d87669e5431
Techboyzep <36669540+Techboyzep@users.noreply.github.com>
2021-05-13 22:36:36 +0530

    hourly commit

---
+++ pract_loops.java
@@ -0,0 +1,15 @@
+
+
+public class pract_loops {
+
+    public static void main(String[] args) {
+        
+        for(double i=0; i<20; i++ ){  //if we know no.of possible iterations for that for loop used..
+
+            System.out.println(i);
+        }
+    }
+
+
+    // Used wen if we dont know the no.of iterations ..
+}
