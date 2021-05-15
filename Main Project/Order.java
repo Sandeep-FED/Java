a867ba4a4cca1c6fa8c86f2ec7134ea5c313bb9c
Techboyzep <36669540+Techboyzep@users.noreply.github.com>
2021-05-15 16:25:49 +0530

    third commit

--- Main Project/Order.java
+++ Main Project/Order.java
@@ -20,7 +20,68 @@ public class Order {
 
         System.out.println("~~~ Access Granted ~~~");
 
+        System.out.println("Are you hungry Yes or No");
 
+        String ans1 = sc.nextLine();
+
+        if (ans1.equals("yes")){
+
+            System.out.println("Choose what you want to eat between: Pizza, Burger or fish");
+
+            String ans2 = sc.nextLine();
+
+            while(!ans2.equals("pizza") && !ans2.equals("burger") && !ans2.equals("fish")){
+
+                System.out.println("Sorry, you choosed wrong choice, we only have Pizza, Burger, Fish");
+
+                ans2 = sc.nextLine();
+            }
+
+            if (ans2.equals("pizza") || ans2.equals("burger") || ans2.equals("pizza")){
+
+                System.out.println("Do you something want to drink Yes or No?");
+                String ans3 = sc.nextLine();
+            
+                while(!ans3.equals("yes") && !ans3.equals("no")){
+
+                    System.out.println("Wrong input, make sure you entered Yes or No");
+                    ans3 = sc.nextLine();
+            }
+
+            if(ans3.equals("yes")){
+                System.out.println("What would you like to drink Coke or Miranda");
+
+                String ans4 = sc.nextLine();
+                while(!ans4.equals("coke") && !ans4.equals("miranda")){
+
+                    System.out.println("Sorry, we dont have it. What would you like between coke & miranda");
+                    ans4 = sc.nextLine();
+                }
+                if(ans4.equals("coke") && ans4.equals("miranda")){
+
+                    System.out.println("Great !! you have ordered " +ans2+ " and " +ans4);
+                }
+
+                System.out.println("Confirm you password: ");
+                pwdConfirm = sc.nextLine();
+
+                while(!pwdConfirm.equals(pwd)){
+                    System.out.println("Enter the correct password: ");
+
+                    pwd = sc.nextLine();
+                }
+
+                System.out.println("Thank you for you order, You will recieve the order");
+            }
+
+
+            }
+        }
+            else {
+
+                System.out.println("Okay, Have a nice day!!");
+            }
     }
-    
+
+
 }
