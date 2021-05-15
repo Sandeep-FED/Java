054dd3fe44998f50c55c386552e6998be12c0da1
Techboyzep <36669540+Techboyzep@users.noreply.github.com>
2021-05-15 16:30:57 +0530

    broken

--- Order.java
+++ Order.java
@@ -7,11 +7,12 @@ public class Order {
         System.out.println("\t Create a Password: ");
         Scanner sc = new Scanner(System.in);
         String pwd = sc.nextLine();
-
         System.out.println("Password created successfully: " +pwd);
+
 
         System.out.println("Enter the Password again: ");
         String pwdConfirm = sc.nextLine();
+        
 
         while(!pwdConfirm.equals(pwd)){
             System.out.println("The entered password is wrong, Please Try again!!:");
@@ -69,6 +70,7 @@ public class Order {
                     System.out.println("Enter the correct password: ");
 
                     pwd = sc.nextLine();
+                    
                 }
 
                 System.out.println("Thank you for you order, You will recieve the order");
